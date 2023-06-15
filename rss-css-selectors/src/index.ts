25a70070483805b4c85616083f140f5c6249c850
Your Name <you@example.com>
2023-06-15 23:57:53 +0300

    init: start task rss-css-selectors

---
+++ rss-css-selectors/src/index.ts
@@ -0,0 +1,6 @@
+import './index.html';
+import './index.scss';
+import exampleLayout from './modules/view/exampleLayout';
+
+const test = new exampleLayout();
+console.log(test.a);
---
+++ rss-css-selectors/src/modules/view/exampleLayout.ts
@@ -0,0 +1,4 @@
+class exampleLayouttst {
+  a = 1;
+}
+export default exampleLayouttst;
