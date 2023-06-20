f0f74ea54da8fa0f851c0c5708c73b15e14233ef
Maxim <maksim.pudeev@bk.ru>
2023-06-20 12:31:20 +0300

    refactor: change all scss files with using mixin, placeholder and variables

--- rss-css-selectors/src/modules/view/htmlViewer.ts
+++ rss-css-selectors/src/modules/view/htmlViewer.ts
@@ -1,7 +1,7 @@
 import { dataHTMLViewer } from '../data/dataExampleLayout';
 class htmlViewer {
   addLayout(data: Record<string, string>, level: number) {
-    const htmlEditor: HTMLElement | null = document.querySelector('.html');
+    const htmlEditor: HTMLElement | null = document.querySelector('.editor__code-html');
     if (htmlEditor) {
       htmlEditor.innerHTML = data[level];
     }
