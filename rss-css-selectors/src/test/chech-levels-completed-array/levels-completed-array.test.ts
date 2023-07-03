b7ef0bd5d3598564749f1c3040810e17b786fc81
Your Name <you@example.com>
2023-07-03 23:50:35 +0300

    refactor: delete comments

--- rss-css-selectors/src/test/chech-levels-completed-array/levels-completed-array.test.ts
+++ rss-css-selectors/src/test/chech-levels-completed-array/levels-completed-array.test.ts
@@ -5,7 +5,6 @@ import CssView from '../../modules/view/css-view';
 describe('render block cssView', () => {
   beforeEach(() => {
     document.body.innerHTML = fs.readFileSync('src/index.html').toString();
-    // const levelsCompleted = [1, 2, 3, 4, 5, 6, 7, 8];
     const level = 0;
     new CssView(dataGameLevels, level);
     expect(document.body).toMatchSnapshot();
@@ -14,16 +13,7 @@ describe('render block cssView', () => {
   });
 
   it('check win game. add congratulation modal', () => {
-    // document.body.innerHTML = fs.readFileSync('src/index.html').toString();
-    // const levelsCompleted = [1, 2, 3, 4, 5, 6, 7, 8];
-    // const level = 0;
-    // new CssView(dataGameLevels, level);
-    // expect(document.body).toMatchSnapshot();
     const levelsCompleted = [1, 2, 3, 4, 5, 6, 7, 8];
-
-    // expect(document.querySelector('.modal.modal__active')).toBeFalsy();
-    // expect(document.querySelector('.modal')).toBeTruthy();
-
     levelsCompleted.push(9);
     if (levelsCompleted.length === dataGameLevels.length) {
       expect(document.querySelector('.modal.modal__active')).toBeTruthy();
@@ -35,16 +25,6 @@ describe('render block cssView', () => {
     const levelsCompletedWithHelp = 5;
     const levelsCompletedWithoutHelp = 4;
     levelsCompleted = levelsCompletedWithHelp + levelsCompletedWithoutHelp;
-    // document.body.innerHTML = fs.readFileSync('src/index.html').toString();
-    // const levelsCompleted = [1, 2, 3, 4, 5, 6, 7, 8];
-    // const level = 0;
-    // new CssView(dataGameLevels, level);
-    // expect(document.body).toMatchSnapshot();
-
-    // expect(document.querySelector('.modal.modal__active')).toBeFalsy();
-    // expect(document.querySelector('.modal')).toBeTruthy();
-
-    // levelsCompleted.push(9);
     if (levelsCompleted === dataGameLevels.length) {
       expect(document.querySelector('.modal.modal__active')).toBeTruthy();
     }
